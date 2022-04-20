a90fad512868f7acd3665c8096e956d93b75c7c2
李江 <lijiang@lijiangdeMacBook-Pro.local>
2022-04-20 17:26:46 +0800

    添加了基于路由的页面导航

---
+++ src/components/Nav/Link.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <!-- eslint-disable vue/require-component-is -->
+  <!-- <component v-bind="linkProps(to)">
+    <slot />
+  </component> -->
+  <router-link :to="to" >
+    <slot />
+  </router-link>
+</template>
+<script lang="ts" >
+import { defineComponent, h } from 'vue'
+export default defineComponent({
+  name: 'AppLink',
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    linkProps(url:string) {
+      // if (isExternal(url)) {
+      //   return {
+      //     is: 'a',
+      //     href: url,
+      //     target: '_blank',
+      //     rel: 'noopener'
+      //   }
+      // }
+      return {
+        is: 'router-link',
+        to: url
+      }
+    }
+  }
+})
+</script>
